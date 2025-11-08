c34323c53b19cc1445324075ecd8204f588649d0
Harshita <harshitaravi31@gmail.com>
2025-11-08 08:48:27 +0530

    finding correlation with scatter plots for eda

--- carPricePrediction/src/explore.py
+++ carPricePrediction/src/explore.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv(r"data\raw\CarPrice_Assignment.csv")
+df = pd.read_csv(r"data/raw/CarPrice_Assignment.csv")
 
 print("First 5 Rows")
 print(df.head())
@@ -20,4 +20,15 @@ print()
 print("Info")
 print(df.info())
 
-sns.pairplot(df)
+#To find correlation
+cols = [i for i in df.columns if df[i].dtype != "object"]
+fig,ax = plt.subplots(len(cols),1,figsize=(6, 6 * len(cols)))
+k=0
+for i in cols:
+    ax[k].scatter(df[i],df["price"])
+    ax[k].set_xlabel(i)
+    ax[k].set_ylabel("price")
+    k+=1
+    
+plt.tight_layout()
+plt.savefig("pairplot of the dataset.png")
